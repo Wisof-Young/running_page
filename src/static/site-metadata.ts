c4cc893ac9445055b68c1ada8eb2edd56e83b1ca
Oceanic <96814993+Wisof-Young@users.noreply.github.com>
2024-03-08 11:01:49 +0800

    init

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -10,8 +10,8 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
+  siteTitle: 'Cycling Page',
+  siteUrl: 'https://run.oceanum.top/',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site and blog',
   navLinks: [
