a8689c08ee78d234272e5774bf49671c178cbdbe
Reese <96814993+Wisof-Young@users.noreply.github.com>
2024-03-08 11:26:51 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -10,14 +10,14 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Cycling Page',
+  siteTitle: '骑行数据',
   siteUrl: 'https://run.oceanum.top/',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site and blog',
   navLinks: [
     {
       name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
+      url: 'https://blog.oceanum.top',
     },
     {
       name: 'About',
